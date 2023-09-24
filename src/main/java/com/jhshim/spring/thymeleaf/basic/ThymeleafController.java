89ee8b809135559090803daf56abfdba6e9ef0f1
jhshim0131 <jhshim0131@gmail.com>
2023-09-24 21:56:56 +0900

    feat:Object, List, Map templating
    Templating Object, List, Map

--- src/main/java/com/jhshim/spring/thymeleaf/basic/ThymeleafController.java
+++ src/main/java/com/jhshim/spring/thymeleaf/basic/ThymeleafController.java
@@ -1,9 +1,13 @@
 package com.jhshim.spring.thymeleaf.basic;
 
+import lombok.Data;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 @Controller
 @RequestMapping("/thymeleaf")
@@ -19,6 +23,39 @@ public class ThymeleafController {
     public String textUnescaped(Model model) {
         model.addAttribute("data", "Hello <b>Spring!</b>");
         return "thymeleaf/text-unescaped";
+    }
+
+    @GetMapping("/variable")
+    public String variable(Model model) {
+        User userA = new User("userA", 10);
+        User userB = new User("userB", 20);
+
+        ArrayList<User> list = new ArrayList<>();
+        list.add(userA);
+        list.add(userB);
+
+        HashMap<String, User> map = new HashMap<>();
+        map.put("userA", userA);
+        map.put("userB", userB);
+
+
+        model.addAttribute("user", userA);
+        model.addAttribute("users", list);
+        model.addAttribute("userMap", map);
+
+        return "thymeleaf/variables";
+    }
+
+    @Data
+    static class User {
+        private String username;
+        private int age;
+
+        public User(String usernamem, int age) {
+            this.username = usernamem;
+            this.age = age;
+        }
+
     }
 
 }
